cb4b8a8965decdfb3bb054da6cccb789fb7de318
SamaraLisboa <samara.bueno@sptech.school>
2024-11-07 18:45:02 -0300

    realizando lista de classe abstrata

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,25 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+
+    private List<Figura> figuras = new ArrayList<>();
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaAreas = 0.0;
+
+        for (Figura figura : figuras) {
+            somaAreas += figura.calcularArea();
+        }
+
+        return somaAreas;
+    }
+
+
 }
